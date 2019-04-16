4aab999177d2ffcd2e53ca7145459a57ca30e5c3
Dave Miller <dm@enfuse.io>
2019-04-16 13:54:22 -0600

    Add HelloWorld endpoint
    This commit adds a Spring Boot Hello World endpoint, with instruction on running locally in a kubernetes cluster

---
+++ src/main/java/com/enfuse/kafka/connect/ConnectApplication.java
@@ -0,0 +1,11 @@
+package com.enfuse.kafka.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectApplication.class, args);
+    }
+}
---
+++ src/test/java/com/enfuse/kafka/connect/ConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enfuse.kafka.connect;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConnectApplicationTests {
+	@Test
+	public void contextLoads() {}
+}
